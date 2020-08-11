912ea7ea7cfcded7317f871fff8b183c88d1dc6e
theDoodleGuy <randomdoodle.pm@googlemail.com>
2020-08-12 00:17:24 +0100

    Added Enemy and navmesh

---
+++ Assets/_Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] GameObject target = null;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+    }
+}
