14ac861247ce7347b2bd1f49a499ee2b1feed2d3
theDoodleGuy <randomdoodle.pm@googlemail.com>
2020-08-12 15:05:35 +0100

    Implemented basic Enemy AI and added gun

--- Assets/_Scripts/EnemyAI.cs
+++ Assets/_Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,7 +7,11 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] GameObject target = null;
+    [SerializeField] float chaseRange = 5f;
+
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     void Start()
     {
@@ -15,6 +20,44 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.transform.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+            ChaseTarget();
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget > navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.transform.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("Attacking Target");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
