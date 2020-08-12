58456acdd3333f859a4b57f98c5056c5f4857bdc
theDoodleGuy <randomdoodle.pm@googlemail.com>
2020-08-12 17:46:38 +0100

    Added muzzle flash

--- Assets/_Scripts/Weapon.cs
+++ Assets/_Scripts/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera = null;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 10f;
+    [SerializeField] ParticleSystem muzzleFlash = null;
 
     void Update()
     {
@@ -18,6 +20,17 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRayCast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRayCast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
@@ -25,7 +38,7 @@ public class Weapon : MonoBehaviour
 
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
 
-           if (target != null)
+            if (target != null)
             {
                 target.TakeDamage(damage);
             }
