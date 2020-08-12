4499c7a0473ddb552f7cfdf60874817eb3434328
theDoodleGuy <randomdoodle.pm@googlemail.com>
2020-08-12 16:35:18 +0100

    Raycast shooting working

---
+++ Assets/_Scripts/EnemyHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    // TODO create a method to reduce hitPoints
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        print("Enemy hp = " + hitPoints);
+    }
+}
---
+++ Assets/_Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera = null;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit " + hit.transform.name);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
